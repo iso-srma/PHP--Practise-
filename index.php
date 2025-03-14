a5b10fed3fd5d26a77cf40a02d95534e51a7483c
iso-srma <isosrma@gmail.com>
2025-03-15 00:03:12 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    echo"I like a burerrr <br>";
+    echo"I like masu and bhatt";
+    //This is a comment 
+    /* This is
+    a multiline
+    comment 
+    */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>Order  masubhaat</button>
+</body>
+</html>
