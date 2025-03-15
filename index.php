a9f322a4a5c5805f92a32aba6f5634a5a8c6a03c
iso-srma <isosrma@gmail.com>
2025-03-15 13:01:00 +0545

    adding practise qn using post or get

--- index.php
+++ index.php
@@ -198,6 +198,38 @@ echo "{$_POST["password"]} <br>";
 
 ?> -->
 
+//practise 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label>Quantity :</label> <br>
+        <input type="text" name="quantity">
+        <input type="submit" value="total">
+    </form>
+</body>
+</html>
+<?php
+    $item="Masu Bhaat";
+    $price=10.5;
+   $quantity = $_POST["quantity"];
+   $total = null;
+
+   $total = $quantity * $price;
+
+   echo "You have ordered {$quantity} * {$item}/s <br>";
+   echo "Your total is : \${$total}";
+    
+?>
+
+
+
 
 
 
