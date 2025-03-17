78a002ec6e68e5ea105060914ae58dd1d6a0d00a
iso-srma <isosrma@gmail.com>
2025-03-17 19:59:37 +0545

    Math function

--- index.php
+++ index.php
@@ -198,8 +198,8 @@ echo "{$_POST["password"]} <br>";
 
 ?> -->
 
-//practise 
 
+<!-- Practise using post order taken form user 
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -226,9 +226,48 @@ echo "{$_POST["password"]} <br>";
    echo "You have ordered {$quantity} * {$item}/s <br>";
    echo "Your total is : \${$total}";
     
+?> -->
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label>x : </label>
+        <input type="text" name="x">
+        <label>y : </label>
+        <input type="text" name="y">
+        <label>z : </label>
+        <input type="text" name="z">
+        <input type="submit" value="total">
+    </form>
+</body>
+</html>
+<?php
+    $x = $_POST["x"];
+    $y = $_POST["y"];
+    $z = $_POST["z"];
+    $total = null;
+
+    // $total=abs($x);
+    // $total =round($x);
+    // $total =floor($x);
+    //$total =ceil($x);
+    // $total = sqrt($x);
+    // $total = pow($x,$y);
+    // $total = max($x,$y,$z);
+    $total = min($x,$y,$z);
+
+    echo $total;
+    
 ?>
-
-
 
 
 
