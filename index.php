26da0acb62ce60b31e348862da39ac7e68bcc923
iso-srma <isosrma@gmail.com>
2025-03-18 23:12:57 +0545

    msg

--- index.php
+++ index.php
@@ -229,8 +229,8 @@ echo "{$_POST["password"]} <br>";
 ?> -->
 
 
-
-
+<!--  
+Day -5 Math Function 
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -269,9 +269,32 @@ echo "{$_POST["password"]} <br>";
     echo $total;
     
 ?>
+   -->
 
+   //practise 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label>radius :</label>
+        <input type="text" name="radius">
+        <input type="submit" value="calculate">
+    </form>
+</body>
+</html>
+<?php
+    $radius = $_POST["radius"];
+    $circum =null;
 
+    $circum =2 * pi() * $radius;
 
+    echo"circum = {$circum}cm <br>";
+?>
 
 
  
