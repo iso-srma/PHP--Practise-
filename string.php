334fa5093d125323c135a2c57ec3afbd5347f974
iso-srma <isosrma@gmail.com>
2025-04-11 16:51:23 +0545

    string

--- string.php
+++ string.php
@@ -1,11 +1,12 @@
 <?php
 $str ="This is a string";
-
 echo $str;
 $lenn =strlen($str);
-
 echo "The length of this string is ";
 echo($lenn);
 
+// echo "The length of the string is  ".$lenn;
+
+echo "<br> The number of words is  in this string is " .str_word_count($str). "Thank you";
 
 ?>
