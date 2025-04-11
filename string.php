6c60bb5813168d1af558ad64d6c787a462be007d
iso-srma <isosrma@gmail.com>
2025-04-11 17:10:45 +0545

    reverse string ,position ,replace string

--- string.php
+++ string.php
@@ -8,5 +8,8 @@ echo($lenn);
 // echo "The length of the string is  ".$lenn;
 
 echo "<br> The number of words is  in this string is " .str_word_count($str). "Thank you";
-
+echo "<br>The reversed sting is :::".strrev($str);
+echo "<br>The search for is in this sting is :::" .strpos($str,"is"). "<br>Thank you";
+echo "<br>The search for is in this sting is :::" .strpos($str,"is"). "<br>Thank you";
+echo "<br>The replace string for 'is' is 'at' in this sting is :::" .str_replace("is","at",$str). "<br>Thank you";
 ?>
